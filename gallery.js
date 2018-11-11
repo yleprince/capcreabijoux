bdaed5c7852f0d9a33b95cff804cc17b5ee22980
yleprince <yrieix.leprince@gmail.com>
2018-11-11 15:03:12 +0000

    last modification

--- gallery.js
+++ gallery.js
@@ -43,50 +43,29 @@ const imgs = [
 	{ path:"1_1_IMG_9294.JPG", style: "normal_img"}
 ];
 
-setTimeout(function() {
- 	document.getElementById("cover_div").style.backgroundImage = "url('images/cover.jpg')"; 
-}, 1000);
 
 imgs.map((img) => gallery.appendChild(create_gallery_item(img.path, img.style)));
 
-let heavy_imgs = imgs.map((img) => {
-	const heavy = document.createElement('img');
-	heavy.id = `h_${img.path}`;
-	heavy.classList.add(img.style);
-	return {target: img.path, img:heavy};
-}
-);
-
-heavy_imgs.map((h) => {
-	h.img.onload = function () {
-		document.getElementById(h.target.split('.')[0]).src = this.src;
-	};
-
+setTimeout (function () {
 	setTimeout(function() {
-		h.img.src = `images/gallery/${h.target}`;
+	 	document.getElementById("cover_div").style.backgroundImage = "url('images/cover.jpg')"; 
 	}, 1000);
+
+	let heavy_imgs = imgs.map((img) => {
+		const heavy = document.createElement('img');
+		heavy.id = `h_${img.path}`;
+		heavy.classList.add(img.style);
+		return {target: img.path, img:heavy};
+	}
+	);
+
+	heavy_imgs.map((h) => {
+		h.img.onload = function () {
+			document.getElementById(h.target.split('.')[0]).src = this.src;
+		};
+
+		setTimeout(function() {
+			h.img.src = `images/gallery/${h.target}`;
+		}, 1000);
+	});
 });
-
-// let diversity_h = document.createElement('img');
-// diversity_h.onload = function() {
-// 	document.getElementById("diversity").src = this.src
-// }
-// setTimeout(function() {
-// 	diversity_h.src = 'images/diversity.jpg';
-// }, 100);
-
-// let flag_h = document.createElement('img');
-// flag_h.onload = function() {
-// 	document.getElementById("flag").src = this.src
-// }
-// setTimeout(function() {
-// 	flag_h.src = 'images/flag.jpg';
-// }, 100);
-
-// let tree_h = document.createElement('img');
-// tree_h.onload = function() {
-// 	document.getElementById("tree").src = this.src
-// }
-// setTimeout(function() {
-// 	tree_h.src = 'images/tree.jpg';
-// }, 100);
